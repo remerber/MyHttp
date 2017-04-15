def0dcad9c24249d07f395429102a18e12b5b5ee
wangwei <275281462@qq.com>
2017-04-15 21:12:57 +0800

    Revert "请求中的处理1、请求异常统一处理  2.token过期需要重新登陆如何统一处理"
    This reverts commit 12bb2d769dd1e23853744f4e3d87bcfcea3357f7.

--- http/src/main/java/com/example/http/OnGlobalExceptionListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.http;
-
-/**
- * Created by HP on 2017/1/17.
- */
-
-public interface OnGlobalExceptionListener {
-    boolean handleException(AppException exception);
-}
--- http/src/main/java/com/example/http/onProgressUpdatedListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.http;
-
-/**
- * Created by HP on 2017/1/16.
- */
-
-public interface onProgressUpdatedListener {
-    void onProgressUpdated(int curLen, int totalLen);
-}
